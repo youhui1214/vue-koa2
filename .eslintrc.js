2ff84a17e41cb91e982abee2ceb9d61816a00cda
youhui1214 <405852890@qq.com>
2020-03-10 22:53:59 +0800

    feat: eslint设置

--- .eslintrc.js
+++ .eslintrc.js
@@ -13,6 +13,7 @@ module.exports = {
     rules: {
         'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
         'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-        indent: ['error', 4],
+        'indent': ['error', 4],
+        'linebreak-style': [0 ,'error', 'windows'],
     },
 };
